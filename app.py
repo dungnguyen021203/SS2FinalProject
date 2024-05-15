0dac77907a2aa84587e5a8224683daae13a710cd
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2024-05-15 16:22:45 +0700

    Dung update 15/5 (no training data)

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, request, jsonify
+import textwrap
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.llms import HuggingFaceHub
 
+import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
-
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-
-
+# Setup
 app = Flask(__name__)
+loader = TextLoader("data.txt")
+document = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(document)
+embeddings = HuggingFaceEmbeddings()
+db = FAISS.from_documents(docs, embeddings)
+llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.8, "max_length": 512})
+chain = load_qa_chain(llm, chain_type="stuff")
 
 
+# Web endpoints
 @app.route("/")
 def index():
     return render_template('chat.html')
@@ -19,27 +30,10 @@ def index():
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
-    msg = request.form["msg"]
-    input = msg
-    return get_Chat_response(input)
-
-
-def get_Chat_response(text):
-
-    # Let's chat for 5 lines
-    for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
-
-        # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-        # generated a response while limiting the total chat history to 1000 tokens,
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-        # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
+    user_query = request.form["msg"]
+    docs_result = db.similarity_search(user_query)
+    answer = chain.run(input_documents=docs_result, question=user_query)
+    return jsonify(answer)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
