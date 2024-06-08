8b30a5b39f5595e915c516b9374e83f002da9ec9
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2024-06-08 22:08:34 +0700

    Final

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACE_API")
 
 # Setup
 app = Flask(__name__)
-loader = TextLoader("data.txt")
-document = loader.load()
+text_loader = TextLoader("data.txt")
+document = text_loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(document)
+docs_final = text_splitter.split_documents(document)
 embeddings = HuggingFaceEmbeddings()
-db = FAISS.from_documents(docs, embeddings)
+db = FAISS.from_documents(docs_final, embeddings)
 llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.7, "max_length": 512})
 chain = load_qa_chain(llm, chain_type="stuff")
 
